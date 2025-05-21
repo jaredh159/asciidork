80b375e49f232cdf685f88cbe30b7dff5f41b2c8
Steve Alexander <steve.alexander@priva.com>
2025-05-21 20:10:54 +0200

    remove unusable line_number_with_offset and source_file_at methods from Parser. tidy up lifetimes in impl ParseResult

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -339,14 +339,6 @@ impl<'arena> Parser<'arena> {
   pub(crate) fn string(&self, s: &str) -> BumpString<'arena> {
     BumpString::from_str_in(s, self.bump)
   }
-
-  pub fn line_number_with_offset(&self, loc: SourceLocation) -> (u32, u32) {
-    self.lexer.line_number_with_offset(loc)
-  }
-
-  pub fn source_file_at(&self, idx: u16) -> &SourceFile {
-    self.lexer.source_file_at(idx)
-  }
 }
 
 pub trait HasArena<'arena> {
@@ -425,7 +417,7 @@ impl Debug for ParseResult<'_> {
   }
 }
 
-impl<'arena> ParseResult<'arena> {
+impl ParseResult<'_> {
   pub fn line_number_with_offset(&self, loc: SourceLocation) -> (u32, u32) {
     self.lexer.line_number_with_offset(loc)
   }
